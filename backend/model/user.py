2d1b604ac6803663171f867761e63f06bda77e92
ernesteNtezirizaza <erneste.ntezirizaza@gmail.com>
2025-02-28 21:01:07 +0200

    Authentication

--- backend/model/user.py
+++ backend/model/user.py
@@ -7,20 +7,12 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    mobile_number = db.Column(db.String(15), nullable=False)
-    location = db.Column(db.String(100), nullable=False)
+    mobile_number = db.Column(db.String(15), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
-    def __init__(self, first_name, last_name, email, mobile_number, location, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
+    def __init__(self, mobile_number, password):
         self.mobile_number = mobile_number
-        self.location = location
-        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')  # Ensure hashing
 
     def check_password(self, password):
         """

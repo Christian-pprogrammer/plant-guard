30c9fa152016cb96fd43da0bb98bdd0222b95ab6
Christian-pprogrammer <mpanoc6@gmail.com>
2025-03-01 04:54:45 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -37,4 +37,4 @@ swagger_ui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={"ap
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)

651fc6a72df35d45da83ba053652a69401fa2e77
Emmy11111 <dufitumukizaemmanuel0@gmail.com>
2025-03-03 10:17:23 +0200

    fix

--- mobile/config/aws.ts
+++
@@ -1,9 +0,0 @@
-import { S3Client } from '@aws-sdk/client-s3';
-
-export const s3 = new S3Client({
-  region: 'us-east-2',
-  credentials: {
-    accessKeyId: 'AKIA2UC3BXLS7IRD4HXD',
-    secretAccessKey: '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',
-  },
-});
